f4c75d9873aea0a178f15ce0c0e1ba28b0aa53d7
Kumar <Kumarroshanofficial@gmail.com>
2023-04-07 12:03:06 +0530

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+V
+&src/test/java/org/example/AppTest.java,4\3\438c56474209176629917f1aaaac4e1cf31f765e
+:
+
+testng.xml,5\2\522a7a4e21649af06297e31a9e33f13bb4fd3022
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+]
+-src/test/java/org/example/GmailLoginTest.java,f\c\fc02c3f8c68f1751961862c9447ad21f723b8eff
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+}
